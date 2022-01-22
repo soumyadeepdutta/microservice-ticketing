4b874b02f4a01acef4ff3f26522185f08c97dd82
soumyadeepdutta <imsoumyadeepdutta@gmail.com>
2022-01-22 23:58:27 +0530

    adding container secrets

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -33,11 +33,15 @@ app.all('*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY not set!!');
+  }
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
     console.log('connected to db');
   } catch (err) {
     console.log(err);
+    process.exit(1);
   }
   app.listen(3000, () => {
     console.log('listening on port 3000!');
